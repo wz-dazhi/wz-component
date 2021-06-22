6a8e0459f34c5e45027e65efe7f8c781befd63b8
wz-dazhi <742820195@qq.com>
2021-06-22 17:04:35 +0800

    继承公共异常

--- wz-qiniu/src/main/java/com/wz/qiniu/exception/QiniuException.java
+++ wz-qiniu/src/main/java/com/wz/qiniu/exception/QiniuException.java
@@ -1,5 +1,6 @@
 package com.wz.qiniu.exception;
 
+import com.wz.common.exception.CommonException;
 import com.wz.common.exception.IErrorCode;
 import com.wz.qiniu.enums.QiniuEnum;
 
@@ -12,36 +13,22 @@ import com.wz.qiniu.enums.QiniuEnum;
  * @date: 2021/5/14
  * @version: 1.0
  */
-public class QiniuException extends RuntimeException {
-
-    private String code;
-
-    private String msg;
+public class QiniuException extends CommonException {
 
     public QiniuException() {
-        this(QiniuEnum.UPLOAD_FAIL);
+        super(QiniuEnum.UPLOAD_FAIL);
     }
 
     public QiniuException(IErrorCode iErrorCode) {
-        this(iErrorCode.getCode(), iErrorCode.getMsg());
+        super(iErrorCode.getCode(), iErrorCode.getMsg());
     }
 
     public QiniuException(String msg) {
-        this(QiniuEnum.UPLOAD_FAIL.getCode(), msg);
+        super(QiniuEnum.UPLOAD_FAIL.getCode(), msg);
     }
 
     public QiniuException(String code, String msg) {
-        super(msg);
-        this.code = code;
-        this.msg = msg;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
+        super(code, msg);
     }
 
 }
