87edf430d83e1e297c4dbb8b591b36b96734f2f9
wz-dazhi <742820195@qq.com>
2022-08-05 16:45:24 +0800

    example

---
+++ wz-component-example/wz-example-datasource-common/src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
+
+/**
+ * @projectName: wz-example
+ * @package: com.example
+ * @className: App
+ * @description:
+ * @author: zhi
+ * @date: 2022/8/4
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) throws SQLException {
+        ConfigurableApplicationContext c = SpringApplication.run(App.class, args);
+        // DataSource ds = c.getBean(DataSource.class);
+        // System.out.println(ds.getConnection());
+    }
+}
