f7de164725fc40829a650160753de4ce1a2c9270
wz-dazhi <742820195@qq.com>
2022-09-29 17:55:21 +0800

    定义SwaggerFilter

--- wz-webmvc/src/main/java/com/wz/webmvc/config/FilterConfig.java
+++ wz-webmvc/src/main/java/com/wz/webmvc/config/FilterConfig.java
@@ -1,6 +1,9 @@
 package com.wz.webmvc.config;
 
+import com.wz.swagger.config.SwaggerProperties;
 import com.wz.webmvc.filter.RequestBodyFilter;
+import com.wz.webmvc.filter.SwaggerFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +28,17 @@ public class FilterConfig {
         registrationBean.addUrlPatterns("/*");
         registrationBean.setName("requestBodyFilter");
         registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
+    }
+
+    @ConditionalOnProperty(name = SwaggerProperties.KNIFE4J_ENABLE_NAME, havingValue = "false", matchIfMissing = true)
+    @Bean
+    public FilterRegistrationBean<SwaggerFilter> swaggerFilterRegistrationBean() {
+        FilterRegistrationBean<SwaggerFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new SwaggerFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setName("swaggerFilter");
+        registrationBean.setOrder(0);
         return registrationBean;
     }
 
