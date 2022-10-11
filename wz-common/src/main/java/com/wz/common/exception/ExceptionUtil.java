d486c85a7f4250a2cf1f5a595da91a6931ff0482
wz-dazhi <742820195@qq.com>
2022-10-11 17:58:09 +0800

    add exception

---
+++ wz-common/src/main/java/com/wz/common/exception/ExceptionUtil.java
@@ -0,0 +1,35 @@
+package com.wz.common.exception;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @projectName: wz-component
+ * @package: com.wz.common.exception
+ * @className: ExceptionUtil
+ * @description:
+ * @author: zhi
+ * @date: 2022/10/11
+ * @version: 1.0
+ */
+@UtilityClass
+public class ExceptionUtil {
+
+    public static RuntimeException wrapRuntime(Throwable t) {
+        return t instanceof RuntimeException ? (RuntimeException) t : new RuntimeException(t);
+    }
+
+    public static CommonException wrapCommon(Exception e) {
+        return new CommonException(e.getMessage());
+    }
+
+    public static CommonException wrapCommon(SystemException e) {
+        return new CommonException(e.getCode(), e.getMsg());
+    }
+
+    public static void wrapAndThrow(Exception e) {
+        if (e instanceof SystemException) {
+            throw wrapCommon((SystemException) e);
+        }
+        throw wrapCommon(e);
+    }
+}
