4ec5a1446e57932be47d275a08f0340ea5b6e8f4
wz-dazhi <742820195@qq.com>
2022-10-24 16:54:55 +0800

    优化Executor

--- wz-web/src/main/java/com/wz/web/config/ExecutorConfig.java
+++ wz-web/src/main/java/com/wz/web/config/ExecutorConfig.java
@@ -1,19 +1,22 @@
 package com.wz.web.config;
 
-import org.slf4j.MDC;
+import com.wz.common.util.StringUtil;
+import com.wz.web.concurrent.AsyncExceptionHandler;
+import com.wz.web.concurrent.ExecutorTaskDecorator;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
 
-import java.util.Map;
+import javax.annotation.Resource;
 import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @projectName: wz
@@ -27,45 +30,55 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 @Configuration
 @EnableConfigurationProperties(ThreadPoolProperties.class)
-public class ExecutorConfig {
+public class ExecutorConfig implements AsyncConfigurer {
 
     @Value("#{ @environment['spring.application.name'] ?: 'app' }")
-    private String applicationName;
+    protected String applicationName;
 
-    /**
-     * 自定义spring线程池
-     */
-    @Bean
-    public Executor executor(ThreadPoolProperties prop) {
+    protected final AtomicInteger threadCount = new AtomicInteger(0);
+
+    @Resource
+    protected ThreadPoolProperties threadPoolProperties;
+
+    @Autowired(required = false)
+    private AsyncUncaughtExceptionHandler asyncUncaughtExceptionHandler;
+
+    @Autowired(required = false)
+    private RejectedExecutionHandler rejectedExecutionHandler;
+
+    @Override
+    public final Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setThreadNamePrefix(applicationName + "-");
-        executor.setCorePoolSize(prop.getCorePoolSize());
-        executor.setMaxPoolSize(prop.getMaxPoolSize());
-        executor.setQueueCapacity(prop.getQueueCapacity());
-        executor.setKeepAliveSeconds(prop.getKeepAliveSecond());
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setThreadFactory(this::createThread);
+        executor.setCorePoolSize(threadPoolProperties.getCorePoolSize());
+        executor.setMaxPoolSize(threadPoolProperties.getMaxPoolSize());
+        executor.setQueueCapacity(threadPoolProperties.getQueueCapacity());
+        executor.setKeepAliveSeconds(threadPoolProperties.getKeepAliveSecond());
+        executor.setRejectedExecutionHandler(rejectedExecutionHandler == null ? new ThreadPoolExecutor.CallerRunsPolicy() : rejectedExecutionHandler);
         executor.setTaskDecorator(new ExecutorTaskDecorator());
         executor.initialize();
         return executor;
     }
 
-    private static class ExecutorTaskDecorator implements TaskDecorator {
-        @Override
-        public Runnable decorate(Runnable r) {
-            // copy MDC context
-            Map<String, String> mdcContext = MDC.getCopyOfContextMap();
-            // copy Request context
-            RequestAttributes requestContext = RequestContextHolder.getRequestAttributes();
-            return () -> {
-                try {
-                    MDC.setContextMap(mdcContext);
-                    RequestContextHolder.setRequestAttributes(requestContext);
-                    r.run();
-                } finally {
-                    MDC.clear();
-                    RequestContextHolder.resetRequestAttributes();
-                }
-            };
-        }
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return asyncUncaughtExceptionHandler == null ? AsyncExceptionHandler.ASYNC_EXCEPTION_HANDLER : asyncUncaughtExceptionHandler;
     }
+
+    protected Thread createThread(Runnable r) {
+        Thread t = new Thread(getThreadGroup(), r, nextThreadName());
+        t.setPriority(threadPoolProperties.getThreadPriority());
+        t.setDaemon(threadPoolProperties.isDaemon());
+        return t;
+    }
+
+    protected String nextThreadName() {
+        String threadNamePrefix = StringUtil.isBlank(threadPoolProperties.getThreadNamePrefix()) ? applicationName : threadPoolProperties.getThreadNamePrefix();
+        return threadNamePrefix + "-" + threadCount.incrementAndGet();
+    }
+
+    protected ThreadGroup getThreadGroup() {
+        return StringUtil.isBlank(threadPoolProperties.getThreadGroupName()) ? null : new ThreadGroup(threadPoolProperties.getThreadGroupName());
+    }
+
 }
