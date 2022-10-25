eae440848e21c9b4fb88c44e85b09cb299b14dbf
wz-dazhi <742820195@qq.com>
2022-10-25 09:34:02 +0800

    test executor

--- wz-webmvc/src/test/java/com/wz/webmvc/WebMvcApp.java
+++ wz-webmvc/src/test/java/com/wz/webmvc/WebMvcApp.java
@@ -1,9 +1,12 @@
 package com.wz.webmvc;
 
+import com.wz.webmvc.async.AsyncService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * @projectName: wz-component
@@ -17,8 +20,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class WebMvcApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ConfigurableApplicationContext c = SpringApplication.run(WebMvcApp.class, args);
+        AsyncService as = c.getBean(AsyncService.class);
+        System.out.println(as.say("张三").get());
+        System.out.println(as.f().get());
+//        as.f();
+//        as.f2();
     }
 
 }
